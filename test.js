776b623f446c79aae8c00b920c222ebe49c31359
cvicens <carlos.vicens.alonso@gmail.com>
2015-12-31 18:39:39 +0100

    Module

--- test.js
+++ test.js
@@ -1,17 +1,30 @@
 var Relay = require('./lib/relay');
 var r1 = new Relay(14);
+var r2 = new Relay(15);
 
 r1.off();
+r2.on();
+
 console.log('antes setInterval');
 var iv = setInterval(function () {
-  var newStatus = r1.readSync() ^ 1;
-  console.log('r1 ' + newStatus);
+  var statusR1 = r1.readSync() ^ 1;
+  console.log('r1 ' + statusR1);
 
-  if (newStatus == 1) {
+  if (statusR1 == 1) {
     r1.on();
   } else {
     r1.off();
   }
+
+  var statusR2 = r2.readSync() ^ 1;
+  console.log('r2 ' + statusR2);
+
+  if (statusR2 == 1) {
+    r2.on();
+  } else {
+    r2.off();
+  }
+
 }, 5000);
 
 console.log('interval = ' + iv);
